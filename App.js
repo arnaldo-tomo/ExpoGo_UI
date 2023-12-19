b7c9b56ad6c4d7a5429ea34e98a94b16d2dcce55
Arnaldo tomo <arnaldotomo@gmail.com>
2023-12-19 09:55:41 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import TabNavigation from './router/TabNavigation';
+import TabNavigation, { Normal } from './router/TabNavigation';
 
 export default function App() {
   return (
-    <TabNavigation />
+    // <TabNavigation />
+    <Normal />
   );
 }
 
