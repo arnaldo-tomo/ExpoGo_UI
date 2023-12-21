de5fcc66557bf71084ea2341265ee0a094dc5776
Arnaldo tomo <arnaldotomo@gmail.com>
2023-12-21 10:11:45 +0200

    Create index.js

---
+++ config/index.js
@@ -0,0 +1,9 @@
+export const darkBg = "#0d1017";
+export const dark = "#161b21";
+export const light = "#ffffff";
+export const darkText = "#cad1d9";
+
+export const StatusDark = "dark";
+export const StatusLight = "light";
+
+
