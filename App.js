b34225ed007e11c7766933385ad685bd400a90f6
Arnaldo tomo <arnaldotomo@gmail.com>
2023-12-21 10:11:40 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import TabNavigation, { Normal } from './router/TabNavigation';
+import { Appearance, useColorScheme } from 'react-native';
+import { Normal } from './router/TabNavigation';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useEffect } from 'react';
 
 export default function App() {
+
+
   return (
-    // <TabNavigation />
     <Normal />
   );
 }
